49f897d9997fb2b4da5a3f51e2c4708bb4bd7fe2
Nicolas Mesa <nicolasmesa@gmail.com>
2018-11-17 15:25:52 -0800

    Adding test and coverage support

--- context_cli/matcher.py
+++ context_cli/matcher.py
@@ -14,33 +14,37 @@ logger = logging.getLogger(__name__)
 class Matcher(ABC):
 
     @abstractmethod
-    def matches(self, line):
+    def matches(self, line): # pragma: no cover
         pass
 
 
 class RegexMatcher(Matcher):
+    """
+    Matcher to match a line against a Regex.
+    """
 
     def __init__(self, regexp):
         self.regexp = build_regexp_if_needed(regexp)
 
     def matches(self, line):
-        return self.regexp.match(line) is not None
+        """
+        Returns True if the line argument matches the regex.
+        """
+        return self.regexp.search(line) is not None
 
 
 class ContainsTextMatcher(Matcher):
+    """
+    Matches a line against a text.
+    """
 
     def __init__(self, text):
         self.text = text
 
     def matches(self, line):
+        """
+        Returns true if the line argument contains the text.
+        """
         return self.text in line
 
-
-class ExactTextMatcher(Matcher):
-
-    def __init__(self, text):
-        self.text = text
-
-    def matches(self, line):
-        return line == self.text
 
--- setup.py
+++ setup.py
@@ -5,6 +5,13 @@ import context_cli
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+test_require = [
+    'pytest',
+]
+setup_requires=[
+    'pytest-runner',
+]
+
 setuptools.setup(
     name="context-cli",
     version=context_cli.__version__,
@@ -21,6 +28,9 @@ setuptools.setup(
             'ctx = context_cli.__main__:main',
         ],
     },
+    test_suite='tests',
+    tests_require = test_require,
+    setup_requires=setup_requires,
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
